9b70eed4653e3a88c0f0cf21a4dba067703620f5
Jeremy English <jhe@jhe-laptop>
2009-04-06 19:45:28 -0500

    More boilerplate.

--- brewconst.py
+++ brewconst.py
@@ -1,3 +1,15 @@
+# Copyright © 2009 Jeremy English <jhe@jeremyenglish.org>
+
+# Permission to use, copy, modify, distribute, and sell this software
+# and its documentation for any purpose is hereby granted without fee,
+# provided that the above copyright notice appear in all copies and
+# that both that copyright notice and this permission notice appear in
+# supporting documentation.  No representations are made about the
+# suitability of this software for any purpose.  It is provided "as
+# is" without express or implied warranty.
+
+# Created: 06-April-2009 
+
 EVPH = 1 #evaperation rate per hour
 MY_EFFICIENCY = 0.65
 GRAIN_TEMP = 70 #Assume room temprature of 70F
--- ingredients.py
+++ ingredients.py
@@ -1,3 +1,15 @@
+# Copyright © 2009 Jeremy English <jhe@jeremyenglish.org>
+
+# Permission to use, copy, modify, distribute, and sell this software
+# and its documentation for any purpose is hereby granted without fee,
+# provided that the above copyright notice appear in all copies and
+# that both that copyright notice and this permission notice appear in
+# supporting documentation.  No representations are made about the
+# suitability of this software for any purpose.  It is provided "as
+# is" without express or implied warranty.
+
+# Created: 06-April-2009 
+
 from grains import *
 
 class Ingredient:
